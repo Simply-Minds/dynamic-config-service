82f279da827cd6f0118d12154653c0289f664084
Abhishek Rathore <abhira719@gmail.com>
2025-02-07 11:24:56 +0530

    IOMS-1006: added boiler plate code for service

---
+++ src/main/java/com/simplyminds/configservice/DynamicConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simplyminds.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplyminds/configservice/DynamicConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplyminds.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
